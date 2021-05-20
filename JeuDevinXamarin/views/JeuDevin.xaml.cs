8e2fc2d8aac8c29dced5f859613198c80e9558cf
Olivier Couvez <o.couvez@free.fr>
2021-05-20 12:16:03 +0200

    first commit

---
+++ JeuDevinXamarin/views/JeuDevin.xaml.cs
@@ -0,0 +1,22 @@
+﻿using JeuDevinXamarin.viewmodels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace JeuDevinXamarin.views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class JeuDevin : ContentPage
+    {
+        public JeuDevin()
+        {
+            InitializeComponent();
+            BindingContext = new JeuDevinViewModel();
+        }
+    }
+}
